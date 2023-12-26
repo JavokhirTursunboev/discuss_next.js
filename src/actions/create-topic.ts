ba9d2e0ffb10141b01d2337e97c5fdf20fc6d42a
Javokhir <jtursunboev@bk.ru>
2023-12-26 09:59:11 +0900

    Fix revalidatePath and redirect calls

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -69,6 +69,6 @@ export async function createTopic(
       };
     }
   }
-  revalidatePath("/");
-  redirect(paths.topicShow(topic.slug));
+  revalidatePath("/"); //learn
+  redirect(paths.topicShow(topic.slug)); //learn
 }
